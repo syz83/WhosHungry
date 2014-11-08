0f573d56629256cc34105e32e3281ddfce5877bc
Steven Zhang <stevenzh2012@gmail.com>
2014-11-08 11:37:01 -0600

    Barebones with parse

---
+++ app/src/main/java/com/example/stevenzhang/whoshungry/MyActivity.java
@@ -0,0 +1,45 @@
+package com.example.stevenzhang.whoshungry;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        Parse.initialize(this, "g69gMNvu3vPoFj8ZMOWFWVFAGaAkB5ci6MfmWc6p", "aS1pEUMZumAOmGVHvZO9trV5DxybBirOq5NWdLq3");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
