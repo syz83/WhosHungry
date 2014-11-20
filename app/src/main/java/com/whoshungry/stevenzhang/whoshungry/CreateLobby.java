afb025aa5492af5678f17c1c9b810387403a95f3
David Emelianov <dsemelianov@utexas.edu>
2014-11-20 10:13:50 -0600

    Added a time picker, as well as buttons to denote lunch/dinner/coffee/drinks

--- app/src/main/java/com/whoshungry/stevenzhang/whoshungry/CreateLobby.java
+++ app/src/main/java/com/whoshungry/stevenzhang/whoshungry/CreateLobby.java
@@ -1,21 +1,195 @@
 package com.whoshungry.stevenzhang.whoshungry;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 
 public class CreateLobby extends Activity {
 
+    private TextView mDisplayTime;
+
+    private Button mTimeButton;
+    private Button mPlaceButton;
+    private Button mInviteButton;
+
+    private Button mLunchButton;
+    private Button mDinnerButton;
+    private Button mCoffeeButton;
+    private Button mDrinksButton;
+
+    private String mLobbyType;
+
+
+    private int hour;
+    private int minute;
+    private String amORpm;
+
+    static final int TIME_DIALOG_ID = 999;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_lobby);
+
+        mDisplayTime = (TextView) findViewById(R.id.button_time);
+
+        addListenerOnButton();
+
     }
 
+    public void addListenerOnButton() {
+
+        mTimeButton = (Button) findViewById(R.id.button_time);
+
+        mTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(TIME_DIALOG_ID);
+            }
+        });
+
+        mPlaceButton = (Button) findViewById(R.id.button_place);
+
+        mPlaceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+
+            }
+        });
+
+        mInviteButton = (Button) findViewById(R.id.button_invite);
+
+        mInviteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+
+            }
+        });
+
+        mLunchButton = (Button) findViewById(R.id.button_lunch);
+        mLunchButton.setBackgroundColor(Color.LTGRAY);
+
+        mLunchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLobbyType = "lunch";
+
+                mLunchButton.setBackgroundColor(Color.DKGRAY);
+
+                mDinnerButton.setBackgroundColor(Color.LTGRAY);
+                mDrinksButton.setBackgroundColor(Color.LTGRAY);
+                mCoffeeButton.setBackgroundColor(Color.LTGRAY);
+            }
+        });
+
+        mDinnerButton = (Button) findViewById(R.id.button_dinner);
+        mDinnerButton.setBackgroundColor(Color.LTGRAY);
+
+        mDinnerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLobbyType = "dinner";
+
+                mDinnerButton.setBackgroundColor(Color.DKGRAY);
+
+                mLunchButton.setBackgroundColor(Color.LTGRAY);
+                mDrinksButton.setBackgroundColor(Color.LTGRAY);
+                mCoffeeButton.setBackgroundColor(Color.LTGRAY);
+
+            }
+        });
+
+        mDrinksButton = (Button) findViewById(R.id.button_drinks);
+        mDrinksButton.setBackgroundColor(Color.LTGRAY);
+
+        mDrinksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLobbyType = "drinks";
+
+                mDrinksButton.setBackgroundColor(Color.DKGRAY);
+
+                mLunchButton.setBackgroundColor(Color.LTGRAY);
+                mDinnerButton.setBackgroundColor(Color.LTGRAY);
+                mCoffeeButton.setBackgroundColor(Color.LTGRAY);
+            }
+        });
+
+        mCoffeeButton = (Button) findViewById(R.id.button_coffee);
+        mCoffeeButton.setBackgroundColor(Color.LTGRAY);
+
+        mCoffeeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLobbyType = "coffee";
+
+                mCoffeeButton.setBackgroundColor(Color.DKGRAY);
+
+                mLunchButton.setBackgroundColor(Color.LTGRAY);
+                mDinnerButton.setBackgroundColor(Color.LTGRAY);
+                mDrinksButton.setBackgroundColor(Color.LTGRAY);
+
+            }
+        });
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case TIME_DIALOG_ID:
+                // set time picker as current time
+                return new TimePickerDialog(this,
+                        timePickerListener, hour, minute,false);
+
+        }
+        return null;
+    }
+
+    private TimePickerDialog.OnTimeSetListener timePickerListener =
+            new TimePickerDialog.OnTimeSetListener() {
+                public void onTimeSet(TimePicker view, int selectedHour,
+                                      int selectedMinute) {
+                    hour = selectedHour;
+                    minute = selectedMinute;
+
+                    convertTimeToDisplay();
+
+                }
+            };
+
+    private void convertTimeToDisplay() {
+        if (hour < 12) {
+            amORpm = "AM";
+        }
+        else { amORpm = "PM"; }
+        if (hour > 12) { hour = hour - 12; }
+        if (hour == 0) { hour = 12; }
+
+        // set current time into textview
+        mDisplayTime.setText(
+                new StringBuilder().append(pad(hour))
+                        .append(":").append(pad(minute))
+                        .append(" ").append(amORpm));
+    }
+
+    private static String pad(int c) {
+        if (c >= 10)
+            return String.valueOf(c);
+        else
+            return "0" + String.valueOf(c);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
